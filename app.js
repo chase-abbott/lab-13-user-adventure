37242f6e61e5aeee1712dbab769fc8ba217e021c
chase-abbott <cabbott93@gmail.com>
2021-04-08 15:36:03 -0700

    add full home page form functionality

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-// import functions and grab DOM elements
+import { setUser } from './local-storage-utils.js';
 
-// initialize state
+const form = document.querySelector('form');
 
-// set event listeners to update state and DOM
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const data = new FormData(form);
+
+    const userClass = data.get('user-class');
+    const userName = data.get('name');
+
+    console.log(userClass)
+    const user = {
+        hp: 35,
+        gold: 0,
+        name: userName,
+        class: userClass,
+        completed: {}
+    }
+
+    setUser(user);
+
+    window.location = './map';
+})
