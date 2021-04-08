e68fcbcead99355bae1e88bb19e31c3a86297b40
chase-abbott <cabbott93@gmail.com>
2021-04-08 15:55:34 -0700

    add redirection to results page if all quests complte

--- map/map.js
+++ map/map.js
@@ -1,6 +1,13 @@
 import { quests } from '../data.js';
+import { getUser } from '../local-storage-utils.js';
 
 const section = document.querySelector('section');
+
+const user = getUser();
+
+if (user.completed['castle'] === true && user.completed['fountain'] && user.completed['cave']) {
+    window.location = '../results';
+}
 
 for (let quest of quests) {
     const anchorTag = document.createElement('a');
@@ -10,3 +17,4 @@ for (let quest of quests) {
 
     section.append(anchorTag);
 }
+
